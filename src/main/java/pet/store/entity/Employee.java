36238031d5b214e9702045479ba2ed2f86861013
Darin Sicurello <darin.sicurello@gmail.com>
2024-12-21 17:47:13 -0700

    Week 13 Review

--- src/main/java/pet/store/entity/Employee.java
+++ src/main/java/pet/store/entity/Employee.java
@@ -1,14 +1,34 @@
 package pet.store.entity;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
 public class Employee {
 
-	PetStore petStore 
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long employeeID;
+ private String employeeFirstName;
+ private String employeeLastName;
+ private String employeePhone;
+ private String employeeJobTitle;
+ 
+ @EqualsAndHashCode.Exclude
+ @ToString.Exclude
 	
-	@ManyToOne(cascade = CascadeType.ALL)
-
-	@JoinColumn(name = "pet_store_id")
-	
-	@Entity
-	
-	 @Data
+@ManyToOne(cascade = CascadeType.ALL)
+@JoinColumn(name = "pet_store_id")
+@ManyToMany(mappedBy= "employees")
+ private PetStore petStore;
 }
--- src/main/java/pet/store/entity/PetStore.java
+++ src/main/java/pet/store/entity/PetStore.java
@@ -1,19 +1,47 @@
 package pet.store.entity;
 
-public class PetStore {
+import java.util.HashSet;
+import java.util.Set;
 
-	Set<Customer> customers
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+
+public class PetStore {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long pet_store_Id;
+	private String pet_store_Name;
+	private String pet_store_address;
+	private String pet_store_city;
+	private String pet_store_state;
+	private String pet_store_zip;
+	private String pet_store_phone;
+
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	Set<Employee> employees = new HashSet<>();
+
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
 	
 	@ManyToMany(cascade = CascadeType.PERSIST)
-
-	@JoinTable(name = "pet_store_customer", joinColumns = @JoinColumn(name = "pet_store_id"), inverseJoinColumns = @JoinColumn(name = "customer_id")
-	
-	Set<Employee> employees
-	
-	@OneToMany(mappedBy = "petStore", cascade = CascadeType.ALL, orphanRemoval = true)
-	
-	@Entity
-	
-	 @Data
-	
+	@JoinTable(name = "pet_store_customer",
+		joinColumns = @JoinColumn(name = "pet_store_id"),
+		inverseJoinColumns = @JoinColumn(name = "customer_id"))
+	private Set<Customer> customers = new HashSet<>();
 }
